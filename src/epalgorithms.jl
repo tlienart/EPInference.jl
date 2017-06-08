9644a4ff41a9e72205a7a3e33584c109e64c7118
Thibaut Lienart <ltib@me.com>
2017-06-08 15:07:33 +0100

    now both alg work, not diag yet

--- src/epalgorithms.jl
+++ src/epalgorithms.jl
@@ -1,6 +1,7 @@
 export
     ParamsEP,
     epNP,
+    epdNP,
     epMP
 
 immutable ParamsEP
@@ -16,7 +17,7 @@ immutable ParamsEP
     nfactors::Int
     function ParamsEP(prior,factors,nEP,nIS,damp=0.1,minvar=1e-5)
         @assert nIS>1 "Need at least two samples per estimator"
-        @assert 0.0<damp<=1.0 "Damping parameter should be in (0,1]"
+        #@assert 0.0<damp<=1.0 "Damping parameter should be in (0,1]"
         new(prior, factors, nEP, nIS, damp, minvar,
             length(mean(prior)), length(factors))
     end
@@ -28,8 +29,13 @@ end
 Serial EP updates with damping in the Natural Parameter space.
 """
 function epNP(p::ParamsEP)
+    globapprox_mem = []
+
     locapprox  = [ones(GaussianNatParam, p.dim)/100 for i in 1:p.nfactors]
     globapprox = p.prior + sum(locapprox)
+
+    push!(globapprox_mem, globapprox)
+
     for iter in 1:p.nEP
         for i in randperm(p.nfactors)
             cavity      = globapprox - locapprox[i]
@@ -40,13 +46,45 @@ function epNP(p::ParamsEP)
             # Cast it as mean parameter
             muhat = GaussianMeanParam(muhat[1], muhat[2])
             # Transform to natural parameter, project in case out of NP space
-            np_muhat = natparam(muhat)
-            # # Perform the damping step
-            globapprox = (1.0-p.damp)*globapprox+p.damp*np_muhat
-            # # Update the global approximation
+            np_muhat     = natparam(project(muhat,minvar=p.minvar))
+            globapprox   = (1.0-p.damp)*globapprox + p.damp*np_muhat
             locapprox[i] = globapprox-cavity
         end
+        push!(globapprox_mem, globapprox)
     end
+    (globapprox, globapprox_mem)
+end
+
+"""
+    epNP
+
+Serial EP updates with damping in the Natural Parameter space.
+"""
+function epdNP(p::ParamsEP)
+    locapprox  = [ones(DiagGaussianNatParam, p.dim)/100 for i in 1:p.nfactors]
+    globapprox = p.prior + sum(locapprox)
+    updatecounter = 0
+    for iter in 1:p.nEP
+        for i in randperm(p.nfactors)
+            cavity      = globapprox - locapprox[i]
+            lltilted(x) = p.logfactors[i](x) + uloglik(cavity, x)
+            (iss,w)     = impsampling(lltilted, globapprox, p.nIS)
+            muhat       = sum( w[j]*suffstats(DiagGaussianNatParam, iss[:,j])
+                                for j in 1:p.nIS )
+            # Cast it as mean parameter
+            muhat = DiagGaussianMeanParam(muhat[1], muhat[2])
+            # Transform to natural parameter, project in case out of NP space
+            np_muhat = natparam(muhat)
+            # Perform the tentative damping step
+            t_globapprox = (1.0-p.damp)*globapprox+p.damp*np_muhat
+            if maximum(-t_globapprox.theta2) < 1.0/p.minvar
+                globapprox   = t_globapprox
+                locapprox[i] = globapprox-cavity
+                updatecounter += 1
+            end
+        end
+    end
+    println("Ratio of accepted updates: $updatecounter/$(p.nEP*p.nfactors)")
     globapprox
 end
 
@@ -56,8 +94,13 @@ end
 Serial EP updates with damping in the Mean Parameter space.
 """
 function epMP(p::ParamsEP)
+    globapprox_mem = []
+
     locapprox  = [ones(GaussianNatParam, p.dim)/100 for i in 1:p.nfactors]
     globapprox = p.prior + sum(locapprox)
+
+    push!(globapprox_mem, globapprox)
+
     for iter in 1:p.nEP
         for i in randperm(p.nfactors)
             cavity      = globapprox - locapprox[i]
@@ -70,6 +113,7 @@ function epMP(p::ParamsEP)
                                   )
             locapprox[i] = globapprox-cavity
         end
+        push!(globapprox_mem, globapprox)
     end
-    globapprox
+    (globapprox, globapprox_mem)
 end
